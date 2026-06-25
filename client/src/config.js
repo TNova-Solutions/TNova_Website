ecebfd3d3fc618beb96fefd118186c6bb425e852
TNova-Solutions <thepakar.murugesan@zohomail.in>
2026-06-25 12:41:45 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL = import.meta.env.PROD
-  ? "https://t-nova-website-delta.vercel.app"
+  ? "https://t-nova-website-hzyx.vercel.app"
   : "https://fuzzy-umbrella-qv7j479j96wjf995g-5000.app.github.dev";
