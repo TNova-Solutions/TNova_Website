ff3cf938d4cf6bf0f94d75c85ae27bdb05f7c89f
Nareshkrishna Murugesan <naresh.krishna@tnovasolutions.com>
2026-06-26 05:05:11 +0000

    bugFix/updated_bugFixes_V1

--- client/src/constants/content.js
+++ client/src/constants/content.js
@@ -15,29 +15,26 @@ export const TERMINAL_LINES = [
 ];
 
 export const CIRCUIT_PULSES = [
-  // Original Pulses
-  { type: "h", pos: "16%", delay: "0s", duration: "6.5s", colorVar: "--emerald-bright" },
-  { type: "h", pos: "54%", delay: "2.4s", duration: "7.2s", colorVar: "--emerald-dim" },
-  { type: "h", pos: "82%", delay: "4.1s", duration: "6s", colorVar: "--emerald-bright" },
-  { type: "v", pos: "20%", delay: "1.1s", duration: "7s", colorVar: "--emerald-dim" },
-  { type: "v", pos: "76%", delay: "3.3s", duration: "6.4s", colorVar: "--emerald-bright" },
-
-  // New Horizontal Pulses
+  // Horizontal Pulses (evenly spaced)
   { type: "h", pos: "8%", delay: "1.5s", duration: "5.8s", colorVar: "--emerald-dim" },
-  { type: "h", pos: "35%", delay: "0.8s", duration: "7.5s", colorVar: "--emerald-bright" },
-  { type: "h", pos: "68%", delay: "3.7s", duration: "6.2s", colorVar: "--emerald-dim" },
-  { type: "h", pos: "92%", delay: "5.2s", duration: "6.8s", colorVar: "--emerald-bright" },
+  { type: "h", pos: "22%", delay: "0.8s", duration: "7.5s", colorVar: "--emerald-bright" },
+  { type: "h", pos: "36%", delay: "2.4s", duration: "7.2s", colorVar: "--emerald-dim" },
+  { type: "h", pos: "50%", delay: "0s", duration: "6.5s", colorVar: "--emerald-bright" },
+  { type: "h", pos: "64%", delay: "3.7s", duration: "6.2s", colorVar: "--emerald-dim" },
+  { type: "h", pos: "78%", delay: "5.2s", duration: "6.8s", colorVar: "--emerald-bright" },
+  { type: "h", pos: "92%", delay: "4.1s", duration: "6s", colorVar: "--emerald-bright" },
 
-  // New Vertical Pulses
-  { type: "v", pos: "12%", delay: "2.1s", duration: "5.5s", colorVar: "--emerald-bright" },
-  { type: "v", pos: "40%", delay: "4.5s", duration: "7.8s", colorVar: "--emerald-dim" },
-  { type: "v", pos: "55%", delay: "0.4s", duration: "6.1s", colorVar: "--emerald-bright" },
-  { type: "v", pos: "88%", delay: "2.8s", duration: "5.9s", colorVar: "--emerald-dim" },
-  { type: "v", pos: "96%", delay: "1.7s", duration: "6.6s", colorVar: "--emerald-bright" },
-
-  { type: "v", pos: "18%", delay: "2.1s", duration: "5.5s", colorVar: "--emerald-bright" },
-  { type: "v", pos: "35%", delay: "4.5s", duration: "7.8s", colorVar: "--emerald-dim" },
-  { type: "v", pos: "70%", delay: "0.4s", duration: "6.1s", colorVar: "--emerald-bright" },
-  { type: "v", pos: "80%", delay: "2.8s", duration: "5.9s", colorVar: "--emerald-dim" },
-  { type: "v", pos: "99%", delay: "1.7s", duration: "6.6s", colorVar: "--emerald-bright" }
+  // Vertical Pulses (evenly spaced)
+  { type: "v", pos: "6%", delay: "2.1s", duration: "5.5s", colorVar: "--emerald-bright" },
+  { type: "v", pos: "14%", delay: "4.5s", duration: "7.8s", colorVar: "--emerald-dim" },
+  { type: "v", pos: "22%", delay: "0.4s", duration: "6.1s", colorVar: "--emerald-bright" },
+  { type: "v", pos: "30%", delay: "2.8s", duration: "5.9s", colorVar: "--emerald-dim" },
+  { type: "v", pos: "38%", delay: "1.7s", duration: "6.6s", colorVar: "--emerald-bright" },
+  { type: "v", pos: "46%", delay: "1.1s", duration: "7s", colorVar: "--emerald-dim" },
+  { type: "v", pos: "54%", delay: "3.3s", duration: "6.4s", colorVar: "--emerald-bright" },
+  { type: "v", pos: "62%", delay: "2.1s", duration: "5.5s", colorVar: "--emerald-bright" },
+  { type: "v", pos: "70%", delay: "4.5s", duration: "7.8s", colorVar: "--emerald-dim" },
+  { type: "v", pos: "78%", delay: "0.4s", duration: "6.1s", colorVar: "--emerald-bright" },
+  { type: "v", pos: "86%", delay: "2.8s", duration: "5.9s", colorVar: "--emerald-dim" },
+  { type: "v", pos: "94%", delay: "1.7s", duration: "6.6s", colorVar: "--emerald-bright" },
 ];
